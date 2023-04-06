53a14094adc522282df577050ef1d064cfb0086f
Smiley98 <smileysofficialcinema@gmail.com>
2023-04-06 13:24:20 -0400

    Added projectile position and rotation

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,6 +1,7 @@
 #include "rlImGui.h"
 #include "Physics.h"
 #include "Collision.h"
+#include <string>
 
 using namespace std;
 
@@ -17,21 +18,32 @@ int main(void)
     const char* greeting = "Hello World!";
     const int textWidth = MeasureText(greeting, fontSize);
 
-    float playerRotation = 0.0f;
+    float playerRotation = -45.0f;
     const float playerRotationSpeed = 100.0f;   // 100 degrees per second
     const float playerDistance = 500.0f;
+
+    const Vector2 originDirection = Direction(0.0f);
+    Vector2 projectilePosition{ -100.0f, 0.0f };
+    Vector2 projectileDirection{};
+    Vector2 playerPosition{};
+    Vector2 playerDirection{};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        float dt = GetFrameTime();
+        const float dt = GetFrameTime();
         if (IsKeyDown(KEY_Q))
             playerRotation -= playerRotationSpeed * dt;
         if (IsKeyDown(KEY_E))
             playerRotation += playerRotationSpeed * dt;
+        if (IsKeyDown(KEY_SPACE))
+        {
+            projectilePosition = playerPosition;
+            projectileDirection = playerDirection;
+        }
 
-        Vector2 playerPosition = GetMousePosition();
-        Vector2 playerDirection = Direction(playerRotation * DEG2RAD);
+        playerPosition = GetMousePosition();
+        playerDirection = Direction(playerRotation * DEG2RAD);
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
@@ -39,12 +51,21 @@ int main(void)
         DrawRectanglePro({ playerPosition.x, playerPosition.y, recWidth, recHeight },
             { recWidth * 0.5f, recHeight * 0.5f }, playerRotation, BLUE);
 
-        DrawRectangle(0, 0, recWidth, recHeight, RED);
+        const float projectileRotation = SignedAngle(originDirection, projectileDirection) * RAD2DEG;
+        DrawRectanglePro({ projectilePosition.x, projectilePosition.y, recWidth, recHeight },
+            { recWidth * 0.5f, recHeight * 0.5f }, projectileRotation, ORANGE);
+
+        //DrawRectangle(0, 0, recWidth, recHeight, RED);
         DrawRectangle(screenWidth - recWidth, 0, recWidth, recHeight, ORANGE);
         DrawRectangle(0, screenHeight - recHeight, recWidth, recHeight, YELLOW);
         DrawRectangle(screenWidth - recWidth, screenHeight - recHeight, recWidth, recHeight, GREEN);
-        
         DrawLineV(playerPosition, playerPosition + playerDirection * playerDistance, BLUE);
+        
+        string angle = "Angle: " + to_string(fmodf(fabs(playerRotation + 360.0f), 360.0f));
+        string dir = "Direction: [" + to_string(playerDirection.x) + ", " +
+            to_string(playerDirection.y) + "]";
+        DrawText(angle.c_str(), 10, 10, fontSize, RED);
+        DrawText(dir.c_str(), 10, 10 + fontSize, fontSize, RED);
         
         EndDrawing();
     }
