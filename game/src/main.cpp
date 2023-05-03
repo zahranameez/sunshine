607f4bccf6dbefa55a0da1c10480b5249a97ce4c
Smiley98 <smileysofficialcinema@gmail.com>
2023-05-03 18:03:47 -0400

    Added position slider

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -5,16 +5,34 @@
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
+    rlImGuiSetup(true);
     SetTargetFPS(60);
 
+    Vector2 position{};
+
+    bool useGUI = false;
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 16, 9, 20, RED);
+
+        DrawRectangleV(position, { 60.0f, 40.0f }, RED);
+
+        if (IsKeyPressed(KEY_GRAVE)) useGUI = !useGUI;
+        if (useGUI)
+        {
+            rlImGuiBegin();
+            ImGui::SliderFloat2("Position", &position.x, 0.0f, SCREEN_WIDTH);
+
+            rlImGuiEnd();
+        }
+
+        DrawFPS(10, 10);
+        DrawText("Press ~ to open/close GUI", 10, 30, 20, GRAY);
         EndDrawing();
     }
 
+    rlImGuiShutdown();
     CloseWindow();
     return 0;
 }
