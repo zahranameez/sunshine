e18779af1d43952f49110b6f0e6ba21ef274e324
Smiley98 <smileysofficialcinema@gmail.com>
2023-05-18 11:36:44 -0400

    Added direction to angle conversions

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,5 +1,6 @@
 #include "rlImGui.h"
 #include "Math.h"
+#include <string>
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
@@ -35,12 +36,17 @@ int main(void)
         if (IsKeyDown(KEY_Q))
             body.direction = Rotate(body.direction, -body.angularSpeed * dt * DEG2RAD);
 
+        float radians1 = atan2(body.direction.y, body.direction.x);
+        float radians2 = SignedAngle({ 1.0f, 0.0f }, body.direction);
+        
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         DrawRectanglePro({ body.position.x, body.position.y, 60.f, 40.0f }, { 30.f, 20.0f },
             SignedAngle({ 1.0f, 0.0f }, body.direction) * RAD2DEG, RED);
         DrawLineV(body.position, body.position + body.direction * 300.0f, BLACK);
+        DrawText(std::to_string(radians1 * RAD2DEG).c_str(), 10, 10, 20, BLACK);
+        DrawText(std::to_string(radians2 * RAD2DEG).c_str(), 10, 30, 20, BLACK);
 
         EndDrawing();
     }
